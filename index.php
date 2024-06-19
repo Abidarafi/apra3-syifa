34af25c3896f8349d83bc5b0ce46f1acd97b2482
Abidarafi <167773413+Abidarafi@users.noreply.github.com>
2024-06-19 19:50:08 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+echo"<center>nama_lengkap: ".$_POST['nama_lengkap']."</center><br>";
+echo"<center>email: ".$_POST['email']."</center><br>";
+echo"<center>nomor_telepon: ".$_POST['nomor_telepon']."</center><br>";
+echo"<center>tanggal_lahir: ".$_POST['tanggal_lahir']."</center><br>";
+echo"<center>alamat_rumah: ".$_POST['alamat_rumah']."</center><br>";
+?>
